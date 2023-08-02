79dfaf102438c6e2adb62fa1272128c77ae78de7
Миролюбов Павел Андреевич <paha472@gmail.com>
2023-08-02 22:54:20 +0300

    Done

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// метод вывода массива
-void PrintArr(int[] arr)
+void PrintArr(string[] arr)
 {
     Console.Write("[");
     for (int i = 0; i < arr.Length - 1; i++)
@@ -12,7 +12,6 @@ void PrintArr(int[] arr)
 //метод получения размерности итогового массива
 int GetResultArrLengrt(string[] arr)
 {
-    string[] resultArr = new string[];
     int len = 0;
     for (int i = 0; i < arr.Length; i++)
     {
@@ -24,4 +23,22 @@ int GetResultArrLengrt(string[] arr)
     return len;
 }
 
+//метод получения итогового массива
+string[] GetResultArr(string[] arr, int resultArrayLength)
+{
+    string[] resultArr = new string[resultArrayLength];
+    int index = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].ToString().ToCharArray().Length < 4)
+        {
+            resultArr[index] = arr[i];
+            index ++;
+        }
+    }
+    return resultArr;
+}
 
+string [] testArr = new string [] {"233123","sdf234","123","e","r4"};
+
+PrintArr(GetResultArr(testArr,GetResultArrLengrt(testArr)));
