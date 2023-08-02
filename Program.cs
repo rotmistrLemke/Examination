5f86a12927631db886fdaf97bd5c79bbba3dd9d1
Миролюбов Павел Андреевич <paha472@gmail.com>
2023-08-02 22:27:51 +0300

    метод вывода массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// метод вывода массива
+void PrintArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ",");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
