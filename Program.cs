daa83fb760f7af8d7fa920eb1e5a0bd2d606204a
Миролюбов Павел Андреевич <paha472@gmail.com>
2023-08-02 22:45:34 +0300

    метод получения размерности итогового массива

--- Program.cs
+++ Program.cs
@@ -9,3 +9,19 @@ void PrintArr(int[] arr)
     Console.WriteLine(arr[arr.Length - 1] + "]");
 }
 
+//метод получения размерности итогового массива
+int GetResultArrLengrt(string[] arr)
+{
+    string[] resultArr = new string[];
+    int len = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].ToString().ToCharArray().Length < 4)
+        {
+            len++;
+        }
+    }
+    return len;
+}
+
+
